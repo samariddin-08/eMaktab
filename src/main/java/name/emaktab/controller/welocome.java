97acf32f6c80d9eb776472675807accbdb6880fd
samariddin-08 <samariddinruziqulov0@gmail.com>
2025-10-19 20:40:29 +0500

    s

---
+++ src/main/java/name/emaktab/controller/welocome.java
@@ -0,0 +1,16 @@
+package name.emaktab.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class welocome {
+
+    @PostMapping
+    public String welocome(){
+        return "Welcome to eMaktab ";
+    }
+}
